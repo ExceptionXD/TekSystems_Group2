2a27dccf0d16842a197fe027cd57b65d919d9237
Surabhi2481 <53371563+Surabhi2481@users.noreply.github.com>
2019-07-28 23:09:29 +0530

    added KNN with n_neighbors=3

--- URL_Model.py
+++ URL_Model.py
@@ -88,3 +88,11 @@ lreg.fit(x_train,y_train)
 model_1 = lreg.score(x_test,y_test)
 model_1
 
+# ## K Nearest Neighbours
+from sklearn.neighbors import KNeighborsClassifier
+classifier = KNeighborsClassifier(n_neighbors=3)
+classifier.fit(x_test, y_test)
+model_2 = classifier.score(x_test, y_test)
+model_2
+
+
